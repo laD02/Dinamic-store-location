0473e6eeeebb952a9d761eaafc092a29197aca7d
LaD02 <lehoanggithub10112002@gmail.com>
2025-11-06 10:20:49 +0700

    update key1

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,59 +1,11 @@
 import type { LoaderFunctionArgs } from "react-router";
-import { redirect, Form, useLoaderData, Navigate } from "react-router";
-
-import { login } from "../../shopify.server";
-
-import styles from "./styles.module.css";
-
+import { redirect } from "react-router";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-
-  if (url.searchParams.get("shop")) {
-    throw redirect(`/app?${url.searchParams.toString()}`);
-  }
-
-  return { showForm: Boolean(login) };
+  // redirect trực tiếp server-side
+  return redirect("/app/help-center"); // hoặc route bạn muốn hiển thị đầu tiên
 };
 
-export default function App() {
-  const { showForm } = useLoaderData<typeof loader>();
-
-  return (
-    <Navigate to="/app" replace />
-    // <div className={styles.index}>
-    //   <div className={styles.content}>
-    //     <h1 className={styles.heading}>A short heading about [your app]</h1>
-    //     <p className={styles.text}>
-    //       A tagline about [your app] that describes your value proposition.
-    //     </p>
-    //     {showForm && (
-    //       <Form className={styles.form} method="post" action="/auth/login">
-    //         <label className={styles.label}>
-    //           <span>Shop domain</span>
-    //           <input className={styles.input} type="text" name="shop" />
-    //           <span>e.g: my-shop-domain.myshopify.com</span>
-    //         </label>
-    //         <button className={styles.button} type="submit">
-    //           Log in
-    //         </button>
-    //       </Form>
-    //     )}
-    //     <ul className={styles.list}>
-    //       <li>
-    //         <strong>Product feature</strong>. Some detail about your feature and
-    //         its benefit to your customer.
-    //       </li>
-    //       <li>
-    //         <strong>Product feature</strong>. Some detail about your feature and
-    //         its benefit to your customer.
-    //       </li>
-    //       <li>
-    //         <strong>Product feature</strong>. Some detail about your feature and
-    //         its benefit to your customer.
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </div>
-  );
+export default function Index() {
+  return null; // Không cần render gì cả
 }
