42687b4a51cb55f17c713f5462821b9d4a5d25c7
LaD02 <lehoanggithub10112002@gmail.com>
2025-12-01 10:15:55 +0700

    update goodle mapp

--- app/utils/geocode.server.ts
+++ app/utils/geocode.server.ts
@@ -8,7 +8,7 @@ export async function getLatLngFromAddress(address: string) {
   try {
     const res = await fetch(url, {
       headers: {
-        "User-Agent": "MyApp/1.0 (your_email@example.com)", // Nominatim yêu cầu header này
+        "User-Agent": "MyApp/1.0 (lehoang10112002@gmail.com)", // Nominatim yêu cầu header này
       },
     });
 
