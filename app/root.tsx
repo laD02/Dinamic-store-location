341654f7410d2145f2c538aa245ebf6a641fb2be
LaD02 <lehoanggithub10112002@gmail.com>
2025-12-01 14:51:18 +0700

    update key map in root

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,11 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "react-router";
+
+export function loader() {
+  return { key: process.env.GOOGLE_MAP_KEY };
+}
 
 export default function App() {
+  const { key } = useLoaderData();
   return (
     <html lang="en">
       <head>
@@ -15,7 +20,7 @@ export default function App() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         />
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA3FrbmZSIybeLabQIyDVqOkap3zEV4IzE&libraries=places"></script>
+        <script src={`https://maps.googleapis.com/maps/api/js?key=${key}&libraries=places`}></script>
         <Meta />
         <Links />
       </head>
